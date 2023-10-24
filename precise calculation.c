42a377e515c138c03fb18c67d399d68b13131075
evgeniy_kiselev <90621422+rvevau@users.noreply.github.com>
2023-10-24 14:42:03 +0300

    Update precise calculation.c

--- precise calculation.c
+++ precise calculation.c
@@ -4,13 +4,14 @@
   /      ------------------- * sin(i * PI/4)
   --       (2i+1) * (2i-1)
   i=1
-*/ 
+*/
 
 #define _USE_MATH_DEFINES
 
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
+#include <locale.h>
 
 #define EPSILON 0.0001
 #define REAL long double
@@ -19,11 +20,11 @@ long double calculateSum(REAL x, long double epsilon) {
 
     REAL sum = 0, term;
 
-    for (unsigned long long int i = 1; i <= INFINITY; i++) {
+    for (unsigned long long int i = 1; ; i++) {
 
         term = ((pow(-1, i) * pow(x, 3 * i + 1)) / ((2 * i + 1) * (2 * i - 1))) * sin(i * M_PI_4);
 
-        if (fabs(term) < epsilon) {
+        if (fabs(term) < DBL_EPSILON && i % 4 != 0) {
 
             break;
 
@@ -41,12 +42,12 @@ int main() {
 
     SetConsoleOutputCP(1253);
 
-    REAL x = 1; //x = 0.995
+    REAL x = 0.995; //x = 0.995
     REAL sum1 = calculateSum(x, EPSILON);
     REAL sum2 = calculateSum(x, DBL_EPSILON);
 
-    printf("\xD3 (\xE5) = %.4f\n", sum1);
-    printf("\xD3 (max \xC4) = %.15f\n", sum2);
+    printf("Σ (ε) = %.4f\n", sum1);
+    printf("Σ (max Δ) = %.15f\n", sum2);
 
     return 0;
 
