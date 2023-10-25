bb17bd8765131666476101d2d9e78d450ee0e3bc
evgeniy_kiselev <90621422+rvevau@users.noreply.github.com>
2023-10-25 16:01:34 +0300

    Update full-square test.c

--- full-square test.c
+++ full-square test.c
@@ -7,13 +7,10 @@ unsigned long long isqrt_ull(unsigned long long x) {
     if (x < 2) {
         return x;
     }
-
-
+    
     unsigned long long s = isqrt_ull(x >> 2) << 1;
     unsigned long long t = s + 1;
-
     return (t > x / t) ? s : t;
-
 }
 
 int main() {
@@ -29,7 +26,6 @@ int main() {
     char* token = strtok(input, " ");
 
     for (unsigned int i = 0; i < n; i++) {
-
         sscanf(token, "%llu", &mass[i]);
         token = strtok(NULL, " ");
     }
@@ -37,35 +33,21 @@ int main() {
     tmp = 0;
 
     for (unsigned int i = 0; i < n; i++) {
-
         if (isqrt_ull(mass[i]) * isqrt_ull(mass[i]) == mass[i] && mass[i] >= tmp) {
-
             tmp = mass[i];
             count++;
-
         }
-
     }
-
     if (count == 0) {
-
         printf("-1");
     }
-
     else {
-
         for (unsigned int i = 0; i < n; i++) {
-
             if (mass[i] == tmp) {
-
                 printf("%d", i);
                 break;
-
             }
         }
-
     }
-
     return 0;
-
 }
