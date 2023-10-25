fb0a913aaee77c1098eb707eae7eceb59f07f8a4
evgeniy_kiselev <90621422+rvevau@users.noreply.github.com>
2023-10-25 15:41:52 +0300

    Update bit representation.c

--- bit representation.c
+++ bit representation.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
-void DecToBin(unsigned int decimal)
-{
-
-	if (decimal >= 2)
-	{
-		DecToBin(decimal / 2);
-	}
-	printf("%d", decimal % 2);
-
+unsigned int dec_bin(unsigned int num) {
+    
+    if(num == 0){
+        printf("0");
+        return 0;
+    }
+    
+   unsigned int bin[32];
+   unsigned int i = 0;
+   while (num > 0) {
+      bin[i] = num % 2;
+      num = num / 2;
+      i++;
+   }
+    
+   for (long long int j = i - 1; j >= 0; j--) {
+      printf("%d", bin[j]);
+   }
+    
+    return 0;
 }
 
-
-int main()
-{
-
-	unsigned int decimal;
-
-	scanf_s("%d", &decimal);
-
-	DecToBin(decimal);
-
-	return 0;
-
+int main() {
+    
+   unsigned int dec;
+   scanf("%d", &dec);
+    
+   dec_bin(dec);
+    
+   return 0;
 }
